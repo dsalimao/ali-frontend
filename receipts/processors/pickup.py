8afdeddbd9a64735811a6457c65cfb7179d5380e
dsalimao <elvamao2017@gmail.com>
2018-10-19 11:10:30 -0700

    tmp use datetime now for receipts time

--- receipts/processors/pickup.py
+++ receipts/processors/pickup.py
@@ -1,7 +1,10 @@
 from receipts.models import Receipts, Item
+from _datetime import datetime
+
 
 def on_pickup(name, raw):
-    r = Receipts(receipts_name=name, raw_content=raw)
+    rdate = datetime.now()
+    r = Receipts(receipts_name=name, receipts_date=rdate, raw_content=raw)
     r.save()
 
 

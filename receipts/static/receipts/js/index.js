9d335bf18043f0c66a74e755ee84f25143e94bc4
dsalimao <elvamao2017@gmail.com>
2018-10-24 23:28:44 -0700

    test google integration

--- receipts/static/receipts/js/index.js
+++ receipts/static/receipts/js/index.js
@@ -30,7 +30,8 @@ function ($scope, $q, $http, $window) {
          var PROJECT_ID = '864443634019';
          var CLIENT_ID = '864443634019-hlqr4tvv33alp9i8t4ig7h1tf6bajl2l.apps.googleusercontent.com';
          var API_KEY = 'AIzaSyDlQ-f2cHGO3NXBNIyNwexefzTb7-EShhw';
-         var SCOPES = 'https://www.googleapis.com/auth/gmail.readonly';
+         var SCOPES = ['https://www.googleapis.com/auth/gmail.readonly',
+         'https://www.googleapis.com/auth/userinfo.email'];
            $window.gapi.client.setApiKey(API_KEY);
            $window.gapi.auth.authorize({
              client_id: CLIENT_ID,
